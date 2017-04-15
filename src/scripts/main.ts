d5cc68249ad0c2de7ef448e104ca72142d4f521e
wakamsha <wakamsha@gmail.com>
2017-04-16 04:49:45 +0900

    chore(main.ts): add comments

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,5 +1,5 @@
 import {Observable} from 'rxjs';
-import {div, label, input, hr, h1, makeDOMDriver} from '@cycle/dom';
+import {div, label, input, hr, h1, VNode, makeDOMDriver} from '@cycle/dom';
 import {run} from '@cycle/rxjs-run';
 import {DOMSource} from '@cycle/dom/rxjs-typings';
 
@@ -7,13 +7,27 @@ type Sources = {
     DOM: DOMSource;
 }
 
-function main(sources: Sources) {
-    const input$ = sources.DOM.select('.field').events('input');
-    const name$ = Observable.from(input$)
+type Sinks = {
+    DOM: Observable<VNode>;
+}
+
+/**
+ * アプリケーション
+ * @param sources
+ * @returns {{DOM: Observable<VNode>}}
+ */
+function main(sources: Sources): Sinks {
+
+    // キー入力イベントを取得 ( Intent )
+    const input$: Observable<Event> = sources.DOM.select('.field').events('input');
+
+    // 入力イベントから現在の状態ないし値を取得 ( Model )
+    const name$: Observable<string> = Observable.from(input$)
         .map((ev: Event) => (ev.target as HTMLInputElement).value)
         .startWith('');
 
-    const vdom$ = name$.map(name => {
+    // 現在の状態を画面に描画 ( View )
+    const vdom$: Observable<VNode> = name$.map(name => {
         return div('.well', [
             div('.form-group', [
                 label('Name: '),
@@ -21,16 +35,19 @@ function main(sources: Sources) {
             ]),
             hr(),
             h1(`Hello ${name}`)
-        ])
+        ]);
     });
 
+    // 結果をドライバに出力する ( Sinks )
     return {
         DOM: vdom$
     };
 }
 
+// アプリケーションからの戻り値を受け取るドライバ群を定義
 const drivers = {
-    DOM: makeDOMDriver('#app-container')
+    DOM: makeDOMDriver('#app-container')  // DOM をレンダリングするドライバ
 };
 
+// アプリケーションとドライバを結びつける
 run(main, drivers);
